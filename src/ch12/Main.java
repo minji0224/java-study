a787dd0e2d910ce54b5b07238e9f46a11ffe4a4c
Minji Choi <minj22222c@gmail.com>
2023-06-28 16:58:19 +0900

    주석 추가

--- src/ch12/Main.java
+++ src/ch12/Main.java
@@ -6,6 +6,6 @@ public class Main {
         MyService srv2 = new MyService("학생 정보 관리 서비스");
 
         srv1.start();    // 스레드 시작
-        srv2.start();
+        srv2.start();     // 다른 스레드 시작
     }
 }
